6b15dc806982059d37e0b867e4982f165eee37dd
AmirHossein Habibvand <ahhabibvand@gmail.com>
2020-04-25 15:51:54 +0430

    Add init script

---
+++ init.sh
@@ -0,0 +1,70 @@
+cd ~/Downloads
+mkdir -p debs
+cd debs
+
+# * Install snaps *
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+
+# * Install uget *
+# sudo add-apt-repository ppa:plushuang-tw/uget-stable
+# sudo apt install ./uget_2.2.1-0ubuntu0_bionic_amd64.deb
+
+cd
+
+sudo apt install git
+sudp apt install curl
+sudo apt install gcc g++ make
+sudo apt install gnome-tweak-tool
+sudo apt install openconnect network-manager-openconnect network-manager-openconnect-gnome
+sudo apt install htop
+
+# * Install snaps *
+sudo snap install code --classic
+sudo snap install skype --classic
+sudo snap install spotify
+sudo snap install vlc
+sudo snap install discord
+
+# * Install node *
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt install -y nodejs
+node --version
+
+# * Install yarn *
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install yarn
+yarn --version
+
+# * Install postgres *
+echo 'deb http://apt.postgresql.org/pub/repos/apt/ focal-pgdg main' | sudo tee /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt install postgresql-12 # or postgresql-10
+
+# * Run postgres server *
+pg_ctlcluster 12 main start
+sudo systemctl start postgresql@12-main
+
+# * Install pip3 *
+sudo apt install python3-pip
+pip3 --version
+
+# * Install fish shell *
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt-get update
+sudo apt-get install fish
+
+# * Set fish as default login shell *
+chsh -s `which fish`
+
+# * Install omf and theme *
+curl -L https://get.oh-my.fish | fish
+fish
+omf install batman
+
+# * Install thefuck *
+sudo pip3 install thefuck
+# thefuck --alias ok 
+# or use fish ok function
