a7b96ec61caa42af21353ee952d7f65c40d9f406
AmirHossein Habibvand <ahhabibvand@gmail.com>
2020-04-25 17:15:17 +0430

    Update activate function

--- fish/functions/activate.fish
+++ fish/functions/activate.fish
@@ -1,13 +1,22 @@
-function activate -d "Activates Python's venv for current directory"
-	if test -e $VIRTUAL_ENV_DIR
-		source (string join / $PWD $VIRTUAL_ENV_DIR 'bin/activate.fish')
+begin
+    set default_path ".venv"
 
-		if contains -- -v $argv
-			echo -s (set_color green) "Python's venv for $PWD activated" (set_color normal)
-		end
-	else
-		if contains -- -v $argv
-			echo (set_color red) "There's no venv found in current directory" (set_color normal)
-		end
-	end
+    function activate -a venv_path, verbose -d "Activates Python's venv for current directory"
+        if test -z "$venv_path"
+            set venv_path $default_path
+        end
+
+        if test -d "$venv_path"
+            source (string join / $PWD $venv_path 'bin/activate.fish')
+
+            if test "$verbose" = '-v'
+                echo -s (set_color green) "Python's venv for $PWD activated" (set_color normal)
+            end
+        else
+            if test "$verbose" = '-v'
+                echo (set_color red) "There's no venv found in current directory" (set_color normal)
+            end
+        end
+    end
+
 end
